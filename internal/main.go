a23f764fa8375ff50c748c9cdf390ca546ea6345
carina-maleski <carinamaleski@gmail.com>
2024-03-19 14:45:22 -0300

    start http server

--- internal/main.go
+++ internal/main.go
@@ -8,16 +8,11 @@ import (
 )
 
 func main() {
-	
-	//start an http server
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-		log.Println(w)
-		log.Println(r)
 	})
 
 	log.Println("Listening on localhost:8080")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
